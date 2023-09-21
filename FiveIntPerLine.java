bd8b04c9a2e55e91f6c839666d7b3c322fad4daa
bhushannani99 <145516648+bhushannani99@users.noreply.github.com>
2023-09-21 15:10:25 -0500

    Create FiveIntPerLine.java

---
+++ FiveIntPerLine.java
@@ -0,0 +1,19 @@
+package assignment;
+
+public class FiveIntPerLine
+{
+    public static void main(String[] args)
+    {
+        int START = 1;
+        int END   = 100;
+        int PER_LINE = 5;
+        for (int i = START; i <= END; i++)
+        {
+            System.out.print(i + " ");
+            if ((i + 1) % PER_LINE == 0)
+                System.out.println();
+        }
+        System.out.println();
+
+    }
+}
