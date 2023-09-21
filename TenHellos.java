8107fdcc1f9bd42cff6ed884549545d5de7253d1
bhushannani99 <145516648+bhushannani99@users.noreply.github.com>
2023-09-21 15:12:10 -0500

    Create TenHellos.java

---
+++ TenHellos.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class TenHellos
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i=0;i<=10;i++)
+        {
+            System.out.println(i + " th Hello");
+        }
+
+    }
+}
